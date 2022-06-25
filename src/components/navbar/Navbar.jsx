59fd981892e612293a7645de46f04a3c69ff1f7b
Hasan <hasan.yilbat@gmail.com>
2022-06-25 13:50:35 +0300

    tour page project

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="#aboutus">About Us</a>
+      <a href="#foryou">For you</a>
+      <a href="#services">Services</a>
+      <a href="#blog">blog</a>
+      <a href="#vlog">vlog</a>
+      <a href="#contact">contact</a>
+    </div>
+  );
+};
+
+export default Navbar;
